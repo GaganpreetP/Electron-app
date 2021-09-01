911a5f9e1ef3fdf42bacb2d1c83dc2c058e93cb5
Gaganpreet Singh <gagans3india@gmail.com>
2021-09-01 17:35:23 +0530

    Updated

---
+++ renderer.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+// const path = require('path');
+
+btnRead = document.getElementById('btnRead');
+
+// let pathName = path.join(__dirname, 'Files');
+
+btnRead.addEventListener('click', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    // let file = path.join(pathName, "abc.txt")
+
+    var data = fs.readFileSync("./Files/abc.txt");
+    console.log(data);
+    console.log("file read");
+
+});
